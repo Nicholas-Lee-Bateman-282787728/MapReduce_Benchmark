430695beda086fc9fc997df3ce00eccf58c050f8
Megan Stiles <megan.e.stiles@gmail.com>
2017-05-01 14:16:12 -0400

    updated RF scripts

--- Spark_MillionSongsRF.py
+++ Spark_MillionSongsRF.py
@@ -15,31 +15,31 @@ if __name__=="__main__":
 	conf = SparkConf().setMaster("local[*]")
 	conf = conf.setAppName("Spark_MillionSongs")
 	sc = SparkContext(conf=conf)
-	data = sc.textFile("s3://ccdatauvamsds2017/YearPredictionMSD.txt")
+	#data = sc.textFile("s3://ccdatauvamsds2017/YearPredictionMSD.txt")
 	#sc.addFile("YearPredictionMSD.txt")
 	#sc.addFile("YearPredictionMSD")
 	
 	#data = SparkFiles.get('YearPredictionMSD.txt')
-	parsedData = data.map(lambda line: array([float(x) for x in line.split(',')]))
+	#parsedData = data.map(lambda line: array([float(x) for x in line.split(',')]))
 
 	#K-Means Code
 	#Sampling with replacement
-	data_sample = parsedData.sample(True, 100, 1234)
+	#data_sample = parsedData.sample(True, 100, 1234)
 
-	start_time = timeit.default_timer()
+	#start_time = timeit.default_timer()
 
-	clusters = KMeans.train(data_sample, 2, maxIterations=10, initializationMode="random")
+	#clusters = KMeans.train(data_sample, 2, maxIterations=10, initializationMode="random")
 
-	time_Kmeans =(timeit.default_timer() - start_time)
-	print(timeit.default_timer() - start_time)
-	time_Kmeans_string = str(time_Kmeans)
-	file = open("s3://ccdatauvamsds2017/output/Time_logs.txt", "w")
-	file.write(time_Kmeans_string)
-	file.close()
+	#time_Kmeans =(timeit.default_timer() - start_time)
+	#print(timeit.default_timer() - start_time)
+	#time_Kmeans_string = str(time_Kmeans)
+	#file = open("s3://ccdatauvamsds2017/output/Time_logs.txt", "w")
+	#file.write(time_Kmeans_string)
+	#file.close()
 
 
 	#Random Forest Code
-	data = MLUtils.loadLibSVMFile(sc, sc.textFile("s3://ccdatauvamsds2017/YearPredictionMSD"))
+	data = MLUtils.loadLibSVMFile(sc, "s3://ccdatauvamsds2017/YearPredictionMSD")
 
 	start_time = timeit.default_timer()
 
