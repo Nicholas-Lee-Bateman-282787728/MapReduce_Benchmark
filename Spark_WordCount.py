3766c84c1a27113fe0d6109ec993e63619b3ce5b
Megan Stiles <megan.e.stiles@gmail.com>
2017-05-02 14:23:05 -0400

    updated scripts

--- Spark_WordCount.py
+++ Spark_WordCount.py
@@ -14,10 +14,10 @@ if __name__=="__main__":
 	sc = SparkContext(conf=conf)
 
 
-    start_time = timeit.default_timer()
+	start_time = timeit.default_timer()
 
-    wordcounts = sc.textFile('s3://ccdatauvamsds2017/Wikidata').map( lambda x: x.replace(',',' ').replace('.',' ').replace('-',' ').lower()).flatMap(lambda x: x.split()).map(lambda x: (x, 1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0])).sortByKey(False)
+	wordcounts = sc.textFile('s3://ccdatauvamsds2017/Wikidata').map( lambda x: x.replace(',',' ').replace('.',' ').replace('-',' ').lower()).flatMap(lambda x: x.split()).map(lambda x: (x, 1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0])).sortByKey(False)
 
-    print(timeit.default_timer() - start_time)
+	print("$$$$$$$$$$$$$$$$$$TOOK THIS LONG:" + timeit.default_timer() - start_time + "$$$$$$$$$$$$")
 
-    sc.stop()
+	sc.stop()
