66a0a18c1dda70a5b25e7ed7e6af3f1514bdc434
asrid93 <asrid93@gmail.com>
2017-05-02 14:05:29 -0400

    Wiki word count

---
+++ Spark_WordCount.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import timeit
+import pyspark
+from numpy import array
+from pyspark import SparkContext
+from pyspark import SparkConf
+from pyspark import SparkFiles
+
+if __name__=="__main__":
+
+	conf = SparkConf().setMaster("local[*]")
+	conf = conf.setAppName("Spark_MillionSongs")
+	sc = SparkContext(conf=conf)
+
+
+    start_time = timeit.default_timer()
+
+    wordcounts = sc.textFile('s3://ccdatauvamsds2017/Wikidata').map( lambda x: x.replace(',',' ').replace('.',' ').replace('-',' ').lower()).flatMap(lambda x: x.split()).map(lambda x: (x, 1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0])).sortByKey(False)
+
+    print(timeit.default_timer() - start_time)
+
+    sc.stop()
